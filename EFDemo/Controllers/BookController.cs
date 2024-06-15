0fb4d8ce42e4269a08017652ddbed4246967bf5d
Meet patel <patelmeet0037@gmail.com>
2024-06-15 23:36:02 +0530

    Latest changes - breaking in BookContoller details

--- EFDemo/Controllers/BookController.cs
+++ EFDemo/Controllers/BookController.cs
@@ -25,6 +25,13 @@ namespace EFDemo.Controllers
         public IActionResult Index()
         {
             List<Book> objList = _context.Books.ToList();
+            foreach (var obj in objList)
+            {
+                //obj.Publisher = _context.Publishers.Find(obj.Publisher_Id);
+
+                //Avoid duplicate calls to the database for the same publisher id
+                _context.Entry(obj).Reference(p => p.Publisher).Load();
+            }
             return View(objList);
         }
 
@@ -72,6 +79,43 @@ namespace EFDemo.Controllers
             }
             return View(obj);
         }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            BookDetail obj = new();
+
+            //edit
+
+            obj = _context.BookDetails.Include(u => u.Book).FirstOrDefault(u => u.Book_Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Details(BookDetail obj)
+        {
+            if (obj.BookDetail_Id == 0)
+            {
+                //create
+                await _context.BookDetails.AddAsync(obj);
+            }
+            else
+            {
+                //update
+                _context.BookDetails.Update(obj);
+            }
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
 
         public async Task<IActionResult> Delete(int id)
         {
